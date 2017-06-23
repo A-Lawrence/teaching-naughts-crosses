329c46155b80660a53cd06d0715a6b526feeee30
Anthony Lawrence <artisan@anthonylawrence.me.uk>
2017-06-23 14:55:49 +0100

    Version 2 - Select column and validate.

--- naughts_crosses.py
+++ naughts_crosses.py
@@ -1,5 +1,9 @@
-# Naughts and Crosses - Version 1
+# Naughts and Crosses - Version 2
 # Alternate between two players.
+# Allow column selection
+# - Validate it's a valid column
+
+COLUMNS = 6
 
 playerTurn = 1
 isWon = False
@@ -12,9 +16,33 @@ def nextTurn():
     else:
         playerTurn = 1
 
+def validateColumn(column):
+    global COLUMNS
+
+    if column < 0:
+        return False
+
+    if column > COLUMNS:
+        return False
+
+    return True
+
+def inputColumnSelection():
+    try:
+        col = int(input("Enter the column (1-6)"))
+
+        if not validateColumn(col):
+            raise ValueError
+    except ValueError:
+        print("Your input wasn't valid.")
+
+        return inputColumnSelection()
+
 while not isWon:
     print("Player %d, it's your turn!" % (playerTurn))
-    input("Press <enter> to take your turn.")
 
+    col = inputColumnSelection()
+
+    print("\n=====NEXT PLAYER=====\n")
     nextTurn()
 
