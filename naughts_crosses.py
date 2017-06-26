b83d0585b9cdcc87ccc4dde76c4c9e85640e1a75
Anthony Lawrence <artisan@anthonylawrence.me.uk>
2017-06-26 11:23:26 +0100

    Added counter to selected column, after validating.

--- naughts_crosses.py
+++ naughts_crosses.py
@@ -1,12 +1,30 @@
-# Naughts and Crosses - Version 2
+# Naughts and Crosses - Version 2.1
 # Alternate between two players.
 # Allow column selection
 # - Validate it's a valid column
+# - Add counter to column
 
 COLUMNS = 6
 
 playerTurn = 1
+playerCounters = { 1 : "O", 2 : "X" }
+board = { }
 isWon = False
+
+def getPlayerCounter():
+    global playerTurn
+    global playerCounters
+
+    return playerCounters[playerTurn]
+
+def prepareBoard():
+    global board
+    global COLUMNS
+
+    for x in range(COLUMNS):
+        board[x+1] = []
+
+    return board
 
 def nextTurn():
     global playerTurn
@@ -33,15 +51,30 @@ def inputColumnSelection():
 
         if not validateColumn(col):
             raise ValueError
+
+        return col
     except ValueError:
         print("Your input wasn't valid.")
 
         return inputColumnSelection()
 
+def addCounter(column):
+    global board
+    global playerTurn
+    global playerCounters
+
+    board[column].append(getPlayerCounter())
+
+prepareBoard()
+
 while not isWon:
-    print("Player %d, it's your turn!" % (playerTurn))
+    print("Player %d, it's your turn! You are %s" % (playerTurn, getPlayerCounter()))
+
+    print(board)
 
     col = inputColumnSelection()
+
+    addCounter(col)
 
     print("\n=====NEXT PLAYER=====\n")
     nextTurn()
